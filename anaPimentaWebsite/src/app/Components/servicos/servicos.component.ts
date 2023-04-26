864b9858ff86942d4b4996a9f4f0726f90ae37f6
Joao Pimenta Giudice <58566178+Azurba@users.noreply.github.com>
2023-04-26 14:10:03 -0600

    add clereamento

--- anaPimentaWebsite/src/app/Components/servicos/servicos.component.ts
+++ anaPimentaWebsite/src/app/Components/servicos/servicos.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./servicos.component.scss']
 })
 export class ServicosComponent {
-   servicos : string[] = ['Odontologia e Clínica Geral', 'Odontología Pediátrica', 'Próteses', 'Harmonização Oro-facial', 'Aplicação de toxina botulínica','Aplicação de Acido hialurônico para preenchimentos e remodelação facial', 'Aplicação de bio estimuladores' , 'Aplicação de fios de PDO para tração e estímulo de colágeno']
-   iconsC : string[] = ['check', 'check', 'check', 'check', 'check', 'check', 'check', 'check']
-   iconsX : string[] = ['x', 'x', 'x', 'check', 'check', 'check', 'check', 'check']
+   servicos : string[] = ['Odontologia e Clínica Geral', 'Odontología Pediátrica', 'Próteses', 'Clareamento Dental' , 'Harmonização Oro-facial', 'Aplicação de toxina botulínica','Aplicação de Acido hialurônico para preenchimentos e remodelação facial', 'Aplicação de bio estimuladores' , 'Aplicação de fios de PDO para tração e estímulo de colágeno']
+   iconsC : string[] = ['check', 'check', 'check' ,'check', 'check', 'check', 'check', 'check', 'check']
+   iconsX : string[] = ['x', 'x', 'x', 'x' ,'check', 'check', 'check', 'check', 'check']
 }
