87d8d70b630d5f1b54b7f26a707b975dba211546
Joao Pimenta Giudice <58566178+Azurba@users.noreply.github.com>
2023-04-26 12:15:22 -0600

    add links

--- anaPimentaWebsite/src/app/Data/AgendamentoOptions.ts
+++ anaPimentaWebsite/src/app/Data/AgendamentoOptions.ts
@@ -5,7 +5,7 @@ export class AgendamentoOptions{
         {
             title: 'Consultorio Particular',
             address: 'Guanabara Office Center - R. Visc. de Taunay, 420 - Vila Itapura, Campinas - SP - Sala 14',
-            img: 'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*',
+            img: './assets/img/7.JPG',
             contact: ['(19)99116-0033'],
             icon: ['phone'],
             button: ['Chame no Whats'],
@@ -15,7 +15,7 @@ export class AgendamentoOptions{
         {
             title: 'Face Doctor - Shopping D. Pedro',
             address: 'Entrada das Pedras: loja S17 Parque Dom Pedro Shopping, Av. Guilherme Campos, 500 - Jardim Santa Genebra, Campinas',
-            img: 'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*',
+            img: './assets/img/8.JPG',
             contact: ['(19)3367-5141'],
             icon: ['phone'],
             button: ['Chame no Whats'],
