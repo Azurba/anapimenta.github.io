bc4e7489c2676c31c586ddcee0b31239690d990b
Joao Pimenta Giudice <58566178+Azurba@users.noreply.github.com>
2023-04-27 09:28:56 -0600

    Add divider

--- anaPimentaWebsite/src/app/app.module.ts
+++ anaPimentaWebsite/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AgendamentoOptions } from './Data/AgendamentoOptions';
 import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
 import { ServicosComponent } from './Components/servicos/servicos.component';
 import { FooterComponent } from './Components/footer/footer.component';
+import {MatDividerModule} from '@angular/material/divider';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { FooterComponent } from './Components/footer/footer.component';
     BrowserAnimationsModule,
     MatIconModule,
     HttpClientModule,
-    MdbCarouselModule
+    MdbCarouselModule,
+    MatDividerModule
   ],
   providers: [AgendamentoOptions],
   bootstrap: [AppComponent]
