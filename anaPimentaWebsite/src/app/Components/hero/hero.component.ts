405f6a2a7e8bb4fdf7744de55216802131785c4f
Joao Pimenta Giudice <58566178+Azurba@users.noreply.github.com>
2023-04-26 18:57:00 -0600

    adjust carousel; add pictures

--- anaPimentaWebsite/src/app/Components/hero/hero.component.ts
+++ anaPimentaWebsite/src/app/Components/hero/hero.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./hero.component.scss']
 })
 export class HeroComponent {
+
+  picturesDesktop : string[] = ['./assets/img/1-1.jpeg', './assets/img/2-1.jpeg', './assets/img/3-1.jpeg', './assets/img/4-1.jpeg'];
+  picturesMobile : string[] =  ['./assets/img/1.jpeg', './assets/img/2.jpeg', './assets/img/3.jpeg', './assets/img/4.jpeg'];
+  usePicturesDesktop: boolean | undefined;
+
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
+
+  @HostListener('window:resize', [])
+  onWindowResize() {
+    this.setPicturesToUse();
+  }
+
+  ngOnInit(): void {
+    this.setPicturesToUse();
+  }
+
+  setPicturesToUse() {
+    const width = window.innerWidth;
+    this.usePicturesDesktop = width > 768; // Set breakpoint as per your requirement
+  }
 
 }
