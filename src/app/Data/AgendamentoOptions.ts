0f5556e61ecab417c737176f4c2ca29a461c5fdb
Joao Pimenta Giudice <58566178+Azurba@users.noreply.github.com>
2023-05-03 16:12:47 -0600

    increase font-size

--- src/app/Data/AgendamentoOptions.ts
+++ src/app/Data/AgendamentoOptions.ts
@@ -6,9 +6,9 @@ export class AgendamentoOptions{
             title: 'Consultorio Particular',
             address: 'Guanabara Office Center - R. Visc. de Taunay, 420 - Vila Itapura, Campinas - SP - Sala 14',
             img: './assets/img/7.JPG',
-            contact: ['(19)99116-0033'],
+            contact: ['(19) 99116-0033'],
             icon: ['phone'],
-            button: ['Chame no Whatsapp'],
+            button: ['Agende uma Consulta'],
             link: 'https://api.whatsapp.com/send/?phone=5519991160033'
         },
 
@@ -16,7 +16,7 @@ export class AgendamentoOptions{
             title: 'Face Doctor - Shopping D. Pedro',
             address: 'Entrada das Pedras: loja S17 Parque Dom Pedro Shopping, Av. Guilherme Campos, 500 - Jardim Santa Genebra, Campinas',
             img: './assets/img/8.JPG',
-            contact: ['(19)3367-5141'],
+            contact: ['(19) 3367-5141'],
             icon: ['phone'],
             button: ['Chame no Whatsapp'],
             link: 'https://api.whatsapp.com/send/?phone=5519996543377'
