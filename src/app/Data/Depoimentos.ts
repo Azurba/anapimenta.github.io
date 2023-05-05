72a1c3791c10019c25d2ca571daaebd26d67fa37
Joao Pimenta Giudice <58566178+Azurba@users.noreply.github.com>
2023-05-04 21:20:38 -0600

    add more depoimentos

--- src/app/Data/Depoimentos.ts
+++ src/app/Data/Depoimentos.ts
@@ -1,4 +1,3 @@
-import { Agendamento } from "../Models/Agendamento";
 import { Depoimento } from "../Models/Depoimento";
 
 export class Depoimentos{
@@ -8,7 +7,7 @@ export class Depoimentos{
             // age: null,
             // city: null,
             // rate: 3,
-            // comment: 'akfjsadl;kfjsa;dlkfjas;lkdfjas;ldkfjas;lkdfjas;lkdfjas;ld',
+            // comment: "",
             // date: '27/04/2022'
             name: 'Maria Bernardete',
             age: 56,
@@ -34,7 +33,49 @@ export class Depoimentos{
             rate: 5,
             comment: '"Dra. Ana, sempre atenciosa e competente."',
             date: '04/05/2023'
-        }
+        },
 
+        {
+            name: 'Francisco Carraro',
+            age: 65,
+            city: "Campinas",
+            rate: 5,
+            comment: '"Ótimo"',
+            date: '04/05/2023'
+        },
+
+        {
+            name: 'Oscar',
+            age: 68,
+            city: "Campinas",
+            rate: 5,
+            comment: '"Solucionou problemas que há anos não eram resolvidos."',
+            date: '04/05/2023'
+        },
+
+        {
+            name: 'Rita Carvalho',
+            age: 66,
+            city: "Campinas",
+            rate: 5,
+            comment: '"Excelente profissional"',
+            date: '04/05/2023'
+        },
+
+        {
+            name: 'Salette',
+            age: 59,
+            city: "Campinas",
+            rate: 5,
+            comment: '"Dra Ana é uma professional muito competente, muito educada, gentil e com muito bom senso. Fui muito bem atendida, sem reclamações. Irei outras vezes e ja indiquei para outras amigas."',
+            date: '04/05/2023'
+        },
     ];
 }
+
+/*
+- I have an angular app when one of the sections will display clients rating about the product. I will display 12 rating at a time. I want that every 10 seconds, 12 new ratings substitute the currents one, without using a carousel. All the ratings are inside of an array and I am using a *ngFor to dynamically display all of them. What are your suggestion on how to do that?
+
+- This way, every 10 seconds, the currentIndex variable is incremented by 12, causing the ngFor loop to display a new set of 12 ratings from the array. When the end of the array is reached, the currentIndex is reset to 0 using the modulo operator, causing the loop to start over from the beginning of the array.
+
+*/
